59bfdacab2c92af8145d801bbb8046c560d187ef
Harry64C <harrisoncooper02@icloud.com>
2024-05-26 15:21:52 -0700

    finished a function that normalizes the testing data

--- Validator.py
+++ Validator.py
@@ -1,19 +1,27 @@
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.euclidean.html#scipy.spatial.distance.euclidean
 # must run pip install scipy first
 from scipy.spatial import distance
+
+import numpy as np 
+import time
 
 class Classifier:
     def __init__(self):
         self = self 
 
     def train(self, filename = "small-test-dataset.txt"): 
+        print("\nStarting training")
+        startTime = time.perf_counter()
+        
         self.dataVals = {} # dictionary that will hold the data
+        self.cols = {}
 
         file = open(filename, 'r')
         data = file.readlines() # read all lines into a list
         instanceIdx = 0
 
         for row in data: # parse the row
+            col = 0
+            
             row = row.split('\n')
             row = row[0].split('  ')
             row.remove('')
@@ -24,13 +32,42 @@ class Classifier:
                 for j in i.split(): # takes care of any whitespace that got through
                     instance.append(float(j)) # python float() converts IEEE to double automatically
 
+                    dataCol = self.cols.get(col, []) # also insert data in columns for later normalization
+                    dataCol.append(float(j))
+                    self.cols[col] = dataCol
+                    col += 1
+
             self.dataVals[instanceIdx] = instance
             instanceIdx += 1
 
         file.close()
+        print("Training took", time.perf_counter() - startTime, "seconds!\n")
+
+
+    def normalize(self):
+        print("Starting data normalization")
+        startTime = time.perf_counter()
+        mean = []
+        std = []
+
+        for i in self.cols.values():
+            # using numpy to easily get the mean and standard deviation for each data column
+            mean.append(np.mean(i))
+            std.append(np.std(i))
+
+        for i in range(0, len(self.dataVals)):
+            row = self.dataVals[i][1:]
+
+            for j in range(0, len(row)):
+                #print("row", j, "was", row[j])
+                self.dataVals[i][j+1] = (row[j] - mean[j]) / std[j]
+
+        #print(self.dataVals)
+        print("Data normalization took", time.perf_counter() - startTime, "seconds!\n")
+
+
 
     def test(self, id, features = []): # returns the expected class of the id
-
         closest = [float('inf'), -1] # initalize closest point as class -1 with distance of infinity
 
         i = 0
@@ -74,8 +111,12 @@ class Validator:
     def __init__(self, dataset = "small-test-dataset.txt"):
         self.classifier = Classifier()
         self.classifier.train(dataset)
+        self.classifier.normalize()
 
     def evaluate(self, features = []):
+        print("Starting accuracy evaluation for feature set", features)
+        startTime = time.perf_counter()
+
         c = self.classifier
         hits, misses = 0, 0
 
@@ -85,7 +126,9 @@ class Validator:
             else:
                 misses += 1
 
+        print("Accuracy evaluation took", time.perf_counter() - startTime, "seconds!\n")
         return hits / (hits + misses)
+        
 
 
 
@@ -98,9 +141,10 @@ def test():
     for i in inp.split():
         features.append(int(i))
 
-    
-    v = Validator("large-test-dataset.txt")
-    print("accuracy is", v.evaluate(features))
+    filename = input("Enter the filename of the dataset you would like to use: ")
+    # large-test-dataset.txt
+    v = Validator(filename)
 
+    print("Accuracy for these features is", v.evaluate(features))
 
 test()
